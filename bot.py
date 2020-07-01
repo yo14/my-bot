388ee8e3e15ece890695703a0264b59386a1d345
yo14 <ifliandry11@gmail.com>
2020-07-01 16:44:40 +0700

    Part 02: Command

--- bot.py
+++ bot.py
@@ -3,6 +3,9 @@ from discord.ext import commands
 
 client = commands.Bot(command_prefix='.')
 
+f = open("rules.txt","r")
+rules = f.readlines()
+
 @client.event
 async def on_ready():
     print('Bot is ready.')
@@ -10,6 +13,10 @@ async def on_ready():
 @client.command()
 async def hello(ctx):
     await ctx.send('Hi testing')
+
+@client.command(aliases=['rules','point'])
+async def rule(ctx, *, number):
+    await ctx.send(rules[int(number) - 1])
 
 TOKEN = 'put_your_token_here'
 
